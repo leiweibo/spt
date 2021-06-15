a83d1f092a049c20d3b4d74bf924fd1d50c45354
leiweibo <leiweibo@gmail.com>
2021-06-16 00:12:43 +0800

    add vue3.0 and element-next support

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <img src="./assets/logo.png">
+    <p>
+      If Element Plus is successfully added to this project, you'll see an
+      <code v-text="'<el-button>'"></code>
+      below
+    </p>
+    <el-button type="primary">el-button</el-button>
+    <el-input v-model="input" placeholder="请输入内容"></el-input>
+    <el-input-number v-model="numInput" @change="handleChange" :min="1" :max="10" label="描述文字"></el-input-number>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import { defineComponent, ref } from 'vue'
 
-export default {
+export default defineComponent ({
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+  },
+  setup() {
+    const numInput = ref(1);
+    const handleChange = (value) => {
+      console.log(value);
+    };
+
+    return {
+      input: ref(''),
+      numInput,
+      handleChange,
+    }
   }
-}
+})
+
 </script>
 
 <style>
---
+++ src/plugins/element.js
@@ -0,0 +1,7 @@
+import { ElButton, ElInput, ElInputNumber } from 'element-plus'
+
+export default (app) => {
+  app.use(ElButton)
+  app.use(ElInput)
+  app.use(ElInputNumber)
+}
