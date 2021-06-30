095628274b9d9ceeb4b54b7c5ac88fc78958a3f6
leiweibo <leiweibo@gmail.com>
2021-06-30 19:11:14 +0800

    add kline chart in the north holding page

--- src/components/north-finance.vue
+++ src/components/north-finance.vue
@@ -24,7 +24,19 @@
     </div>
     <el-button type="primary" v-on:click="getData()">股票北向资金持仓</el-button>
     <div id="err">{{ errMsg }}</div>
-    <div id="chars-box" style="width: 100%; height: 400px;">
+    <!-- <el-space wrap>
+      <div id="chars-box" style="width: 100%; height: 400px;">
+      </div>
+      <Layout :title='chartTitleStock'>
+        <div id="chart-type-stock-k-line" class="k-line-chart"/>
+      </Layout>
+    </el-space> -->
+    <div id="output">
+      <div id="content" wrap>
+        <div id="chars-box" style="width: 100%; height: 400px;">
+        </div>
+        <div id="chart-type-stock-k-line" style="width: 100%; height: 400px; margin-top: 50px"/>
+      </div>
     </div>
   </div>
 </template>
@@ -36,6 +48,7 @@ import dayjs from "dayjs";
 import { aliyunGet } from "../service/http";
 import { dateFormat } from "../utils/format";
 import { dorequest } from '../utils/ftservice';
+import { init } from 'klinecharts'
 
 export default {
   name: "northFinance",
@@ -72,7 +85,6 @@ export default {
       chartTitleStock: ''
     };
   },
-
   methods: {
     getData: async function() {
       this.websocket = dorequest(this.$store, async (msg) => {
@@ -88,8 +100,8 @@ export default {
         const holdingMap = new Map();
         // const finalHoldingMap = new Map();
 
-        // const securityLineRaw = await this.fetchHistoryKline({"code": this.code, "market": this.market})
-
+        const securityLineRaw = await this.fetchHistoryKline({"code": this.code, "market": this.market})
+        this.setupKline(securityLineRaw, this.stKLineChart)
         northholding.data.rows.map((data) => {
           holdingMap.set(dayjs(data.trade_date).format('YYYYMMDD'), data.holding_amt);
         })
@@ -106,8 +118,6 @@ export default {
         //   klineMap.set(date, data.closePrice * 3000000);
         // })
 
-        
-        let myChart = echarts.init(document.getElementById("chars-box"), "dark");
         const holdingData = Array.from(holdingMap.values());
 
         const klineData = Array.from(klineMap.values());
@@ -115,7 +125,7 @@ export default {
         console.log(`the legth of holdingData is ${holdingData.length}`)
         console.log(`the legth of klineData is ${klineData.length}`)
 
-        myChart.setOption({  //动画的配置
+        this.myChart.setOption({  //动画的配置
           series: [
           {
             smooth: true,
@@ -137,7 +147,7 @@ export default {
 
         window.onresize = function() {
           //自适应大小
-          myChart.resize();
+          this.myChart.resize();
         };
       }, 
       (msg) => {
@@ -174,9 +184,122 @@ export default {
           console.log("err:", err);
         });
     },
+
+    setupKline(rawLine, kLineChart) {
+      const plateLine = rawLine.map((item) => {
+          return {
+            close: item.closePrice,
+            high: item.highPrice,
+            low: item.lowPrice,
+            open: item.openPrice,
+            timestamp: item.timestamp * 1000,
+            turnover: item.turnover,
+            volume: item.volume,
+            changeRate: item.changeRate
+          };
+        });
+        console.log(`the plateLine is: ${plateLine}`)
+        kLineChart.setStyleOptions({
+          xAxis: {
+            show: true,
+            height: 30,
+            axisLine: {
+              show: true,
+              color: '#888888',
+              size: 1
+            },
+            tickText: {
+              show: true,
+              color: '#D9D9D9',
+              family: 'Helvetica Neue',
+              weight: 'normal',
+              size: 12,
+              paddingTop: 3,
+              paddingBottom: 6
+            },
+            tickLine: {
+              show: true,
+              size: 1,
+              length: 3,
+              color: '#888888'
+            }
+          },
+          candle: {
+            bar: {
+              upColor: '#EF5350',
+              downColor: '#26A69A',
+              noChangeColor: '#888888'
+            },
+            // 提示  
+            tooltip: {
+              // 'always' | 'follow_cross' | 'none'
+              showRule: 'follow_cross',
+              // 'standard' | 'rect'
+              showType: 'rect',
+              labels: ['时间', '开', '收', '高', '低', '成交量', '涨跌幅'],
+              // 可以是数组，也可以是方法，如果是方法则需要返回一个数组
+              // 数组和方法返回的数组格式为:
+              // [xxx, xxx, ......]或者[{ color: '#fff', value: xxx }, { color: '#000', value: xxx }, .......]
+              values: kLineData => {
+                return [
+                  {
+                    value: kLineData['timestamp']
+                  },
+                  {
+                    value: kLineData['open']
+                  },
+                  {
+                    value: kLineData['close']
+                  },
+                  {
+                    value: kLineData['high']
+                  },
+                  {
+                    value: kLineData['low']
+                  },
+                  {
+                    value: kLineData['volume']
+                  },
+                  {
+                    value: kLineData['changeRate']
+                  },
+                ]
+              },
+              defaultValue: 'n/a',
+              rect: {
+                paddingLeft: 0,
+                paddingRight: 0,
+                paddingTop: 0,
+                paddingBottom: 6,
+                offsetLeft: 8,
+                offsetTop: 8,
+                offsetRight: 8,
+                borderRadius: 4,
+                borderSize: 1,
+                borderColor: '#e9ebef',
+                fillColor: 'rgba(17, 17, 17, .3)'
+              },
+              text: {
+                size: 12,
+                family: 'Helvetica Neue',
+                weight: 'normal',
+                color: '#ffffff',
+                marginLeft: 8,
+                marginTop: 6,
+                marginRight: 8,
+                marginBottom: 0
+              }
+            }
+          }
+        })
+        kLineChart.applyNewData(plateLine)
+    }
   },
 
-
+  mounted: function () {
+    this.stKLineChart = init('chart-type-stock-k-line')
+    this.myChart = echarts.init(document.getElementById("chars-box"), "dark");
+  },
 
   setup() {
     /// 声明定义一下echart
@@ -262,4 +385,20 @@ export default {
       border: 1px solid #d8dfe6;
     }
   }
+  .k-line-chart-container {
+    display: flex;
+    flex-direction: column;
+    margin: 15px;
+    border-radius: 2px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, .3);
+    background-color: #FFFFFF;
+    width: 620px;
+    height: 440px;
+    padding: 16px 6px 16px 16px;
+  }
+
+.k-line-chart {
+  display: flex;
+  flex: 1;
+}
 </style>
