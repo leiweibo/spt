831a22f3b9c2c0c10011a7f3c6f8d8529ac34726
leiweibo <leiweibo@gmail.com>
2021-08-20 23:45:03 +0800

    complete the strategy page

--- src/components/northbonds-strategy.vue
+++ src/components/northbonds-strategy.vue
@@ -2,8 +2,21 @@
   <div id="main">
     <div id="input">
       <el-form :inline=true>
+        <el-form-item label="股票代码">
+          <el-input id="code" v-model="code" placeholder="SH或者SZ开头"></el-input>
+        </el-form-item>
+        <el-form-item label="起始资金">
+          <el-input id="initBalance" v-model="initBalance" placeholder="起始资金"></el-input>
+        </el-form-item>
+        <el-form-item label="每次购买金额比例">
+          <el-input id="buyRatio" v-model="buyRatio" placeholder="格式为0.01, 表示10%"></el-input>
+        </el-form-item>
+        <el-form-item label="每次卖出比例">
+          <el-input id="sellRatio" v-model="sellRatio" placeholder="格式为0.01, 表示10%"></el-input>
+        </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="onSubmit">执行</el-button>
+          <el-button type="primary" @click="onSubmit"
+           v-loading.fullscreen.lock="fullscreenLoading">执行</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -30,6 +43,11 @@ export default {
     return {
       chart_box_id: this.newDate(),
       errMsg: '',
+      code: null, 
+      initBalance: null,
+      buyRatio: null,
+      sellRatio: null,
+      fullscreenLoading: false,
     }
   },
   methods: {
@@ -37,24 +55,28 @@ export default {
       return dayjs().millisecond;
     },
     executeStrategy: async function() {
-      const strategyResult = await aliyunGet(`/strategy/1`)
+      const strategyResult = await aliyunGet(`/strategy/1`, { code: this.code, init: this.initBalance, buyRatio: this.buyRatio, sellRatio: this.sellRatio })
+      this.fullscreenLoading = false;
       const profits = strategyResult.data.map((data) => data.profit);
       const initPrice = strategyResult.data[0].close;
+      
       const prices = strategyResult.data.map((data) => {
         return ((data.close - initPrice)/initPrice).toFixed(3);
       });
+      console.log(profits);
+      console.log(prices);
       this.strategyResultChart.setOption({
         legend: {
-          data: ['收益率', '价格(SH510500)']
+          data: ['策略收益率', '指数收益率']
         },
         series: [
           {
-            name: '收益率',
+            name: '策略收益率',
             type: 'line',
             data: profits
           },
           {
-            name: '收益率(SH510500)',
+            name: '指数收益率',
             type: 'line',
             data: prices,
             yAxisIndex: 1,
@@ -65,18 +87,24 @@ export default {
         },
         yAxis: [
           {
-            name: "收益率",
+            name: "策略收益率",
             type: "value",
+            min: -0.20,
+            max: 0.3,
+            splitNumber: 5,
             splitLine:{
-                 show:false
-             }
+              show:false
+            }
           },
           {
-            name: "收益率(SH510500)",
+            name: "指数收益率",
             type: "value",
+            min: -0.20,
+            max: 0.3,
+            splitNumber: 5,
             splitLine:{
-                 show:false
-             }
+              show:false
+            }
           },
           
         ],
@@ -90,14 +118,12 @@ export default {
     },
     onSubmit: async function() {
       this.fullscreenLoading = true;
-      this.passedData = {};
-      this.ccode = '';
-      await this.executeStrategy(this.ccode, 1);
+      await this.executeStrategy(this.ccode, this.initBalance, this.buyRatio, this.sellRatio);
     },
   },
 
   mounted: function() {
-    this.strategyResultChart = echarts.init(document.getElementById(this.chart_box_id), "dark");
+    this.strategyResultChart = echarts.init(document.getElementById(this.chart_box_id));
   },
 
   setup() {
